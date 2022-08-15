37d112d6220ac34e9ef449da8bb3e7fad28bfd09
xzlh <d.s.macdonald@protonmail.ch>
2022-08-15 14:15:07 +0100

    create terraform directory and bucket A module

---
+++ terraform/s3_upload/s3.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name = var.bucket_alias
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id  
+  acl = "private"
+}
---
+++ terraform/s3_upload/vars.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  type = string
+  default = "gel-test-bucket-target"
+}
+
+variable "bucket_alias" {
+  type = string
+  default = "upload"
+}
